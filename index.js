93196a38d19d91d5417ef5973fc909ab579d33f5
phibersoft <phibersoft@hotmail.com>
2020-09-28 02:22:54 +0300

    oprt

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const vhost = require("vhost");
 const express = require("express");
-const app_one = require("./app_one");
-const app_two = require("./app_two");
-express()
-  .use(vhost(`specdown.com`, app_one))
-  .use(vhost(`admin.specdown.com`, app_two));
+// const app_one = require("./app_one");
+// const app_two = require("./app_two");
+// express()
+//   .use(vhost(`specdown.com`, app_one))
+//   .use(vhost(`admin.specdown.com`, app_two));
+var app = express();
+var PORT = process.env.PORT || 4000;
+app.get(`/`, (req, res) =>
+  res.json({ success: true, developer: "phibersoft" })
+);
+
+app.listen(PORT, () => console.log(`Listening on Port : ${PORT}`));
